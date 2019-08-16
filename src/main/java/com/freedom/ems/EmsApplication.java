dc3115bb72103f455cb9f6c4edcaaba13853d7e7
Saurabh Dedhia <saurabh.dedhia@pearson.com>
2019-08-16 10:33:13 -0700

    Initial Commit

---
+++ src/main/java/com/freedom/ems/EmsApplication.java
@@ -0,0 +1,13 @@
+package com.freedom.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsApplication.class, args);
+	}
+
+}
